97d8e08e093d2ab9df99a5027718ddd24f89c683
Rishi Satsangi <rishi.satsangi@gmail.com>
2018-04-10 14:24:43 -0400

    react router

--- src/frontend/js/pages/index.js
+++ src/frontend/js/pages/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { IndexRoute, Router, Route, browserHistory } from 'react-router';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom';
 // import { Provider } from 'react-redux';
 
 // import App from 'components/app';
@@ -14,21 +18,38 @@ import { IndexRoute, Router, Route, browserHistory } from 'react-router';
 //     browserHistory.replace(lastRequestUri);
 // }
 
+const Home = () => (
+    <div>
+        <h2>Home</h2>
+    </div>
+)
+
+const About = () => (
+    <div>
+        <h2>About</h2>
+    </div>
+)
+
+const Topic = ({ match }) => (
+    <div>
+        <h3>{match.params.topicId}</h3>
+    </div>
+)
+
 render((
-    <div>farts</div>
+    <Router>
+        <div>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/topic/47">Topics</Link></li>
+            </ul>
+
+            <hr />
+
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/topic/:topicId" component={Topic} />
+        </div>
+    </Router>
 ), document.getElementById('mount'));
-
-// render((
-//     <Provider store={ store }>
-//         <Router history={ browserHistory } onUpdate={ logPageView }>
-//             <Route path="/" component={ App } props={ { securityContext } }>
-//                 <IndexRoute component={ Index } props={ { securityContext } } />
-
-//                 /** external */
-//                 <Route path="playlists" component={ Playlists } props={ { securityContext } } />
-
-//                 <Route path="*" component={ Index } props={ { securityContext } } />
-//             </Route>
-//         </Router>
-//     </Provider>
-// ), document.getElementById('mount'));
